7eb9b396ada96979d0c768cc8deefd8036ec84fa
ptejas26 <ptejas.patelia@gmail.com>
2023-11-06 16:02:30 -0500

    Initial Commit

---
+++ SCrollViewDemoUITests/SCrollViewDemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SCrollViewDemoUITestsLaunchTests.swift
+//  SCrollViewDemoUITests
+//
+//  Created by Tejas on 2023-11-06.
+//
+
+import XCTest
+
+final class SCrollViewDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
